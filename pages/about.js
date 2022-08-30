60101efce51b1df12d42ef813038bd0347c6e689
Eduardo <rcchristiane@gmail.com>
2022-08-29 21:29:35 -0500

    Clase de rutas básicas de NextJS

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+
+    return (
+        <div>
+            Esta e la pagina de about<br/>
+            S&iacute; o no raza!!
+        </div>
+    )
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Reac from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hola Hommies!</h1>
+        </div>
+    )
+}
+
+export default Home;
